e8f9be16db30436e9be0b0c60caa47a925e3a621
Li Jian <lijian@hantek.com>
2018-08-14 13:15:18 +0800

    初始化权重矩阵

--- part2_neural_network.py
+++ part2_neural_network.py
@@ -1,5 +1,7 @@
 # python notebook for Make Your Own Neural Network
 
+import numpy
+
 # neural network class definition
 class neuralNetwork:
     
@@ -7,28 +9,40 @@ class neuralNetwork:
     def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
         # set number of nodes in each inpput, hidden, output layer
         self.inodes = inputnodes
-        self.hondes = hiddennodes
+        self.hnodes = hiddennodes
         self.onodes = outputnodes
         
         # learning rate
         self.lr = learningrate
+
+        # link weight matirces, wih and who
+        # weights inside tha arrays are w_i_j, where link is from node i to node j int eh next layer
+        # w11 w21
+        # w12 w22 etc
+        self.wih = numpy.random.rand(self.hnodes, self.inodes) - 0.5
+        self.who = numpy.random.rand(self.onodes, self.hnodes) - 0.5
     
     # train the neural network
-    def train():
+    def train(self):
         pass
     
     # query the neural network
-    def query():
+    def query(self):
         pass
+
+    def __str__(self):
+        return "wih:" +str(self.wih) + "\n" + \
+        "who:" + str(self.who) + "\n"
 
 # 使用我们创建的神经网络类
 # number of input, hidden and output nodes
-input_nodes = 3
+input_nodes = 2
 hidden_nodes = 3
-output_nodes = 3
+output_nodes = 1
 
 #learning rate is 0.3
 learning_rate = 0.3
 
 #create instance of neural network
 n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_rate)
+print("n:"+str(n))
